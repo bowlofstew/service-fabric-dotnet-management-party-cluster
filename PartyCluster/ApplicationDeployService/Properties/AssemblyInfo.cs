e153e31f25178259f4899a45b5dd9219b2d39b3d
vturecek <vturecek@microsoft.com>
2015-11-16 23:21:20 -0800

    Prepending EXE name with PartyCluster for consistency. This also keeps the service EXEs grouped nicely when debugging locally.

--- PartyCluster/ApplicationDeployService/Properties/AssemblyInfo.cs
+++ PartyCluster/ApplicationDeployService/Properties/AssemblyInfo.cs
@@ -11,11 +11,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("ApplicationDeployService")]
+[assembly: AssemblyTitle("PartyCluster.ApplicationDeployService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ApplicationDeployService")]
+[assembly: AssemblyProduct("PartyCluster.ApplicationDeployService")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
